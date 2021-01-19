c7a0036036d265bfb388e8497e1af4c3f53ee6ca
monikacil <monika.cilinska@gmail.com>
2021-01-19 21:08:55 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [showModal, setShowModal] = React.useState(false);
+  const refModal = React.useRef(null);
+  const hideModal = () => setShowModal(false);
+
+  const handleClick = () => alert('Zdarzenie po kliknięciu przycisku Send')
+
+  const message = 'Przykładowy komponent okna dialogowego w aplikacji React.'
+
+  React.useEffect(() => {
+      const modalNode = refModal.current;
+
+      if (!modalNode) {
+          return;
+      }
+
+      modalNode.addEventListener('close', hideModal);
+      modalNode.addEventListener('btn-click', handleClick);
+       
+      return () => {
+          modalNode.removeEventListener('close', hideModal);
+          modalNode.removeEventListener('btn-click', handleClick);
+      };
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button type="button" onClick={() => setShowModal(true)}>
+          Open modal
+        </button>
+        <custom-modal ref={refModal} 
+                      title="Modal Web Components" 
+                      button="Send"
+                      size="l"
+                      open={showModal}>
+          <div slot="content">
+            <p>{message}</p>
+          </div>
+        </custom-modal>
     </div>
   );
-}
+};
 
 export default App;
